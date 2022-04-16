9f4db74081fec1bfc57b21cf968b0a6fc40b7628
jafarjtown <jafaridris82@gmail.com>
2022-04-16 14:58:52 +0100

    authentication++

--- account/urls.py
+++ account/urls.py
@@ -5,10 +5,10 @@ from rest_framework_simplejwt import views as jwt_views
 
 app_name = 'account'
 urlpatterns = [
-    path('api/user/', views.Login.as_view(), name='user_login'),
-    path('api/user/test', views.Home.as_view(), name='test'),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/', views.Login.as_view(), name='user_login'),
+    path('test/', views.Home.as_view(), name='test'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
  
 ]
    
