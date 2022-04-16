d762b3a512e889558f2a7c0d9315c1722211f666
jafarjtown <jafaridris82@gmail.com>
2022-04-16 15:02:57 +0100

    authentication++

--- platon_backend/urls.py
+++ platon_backend/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
-
+def Index(request):
+    return HttpResponse('<h1>PlatON Server</h1> <br/> <p>welcome to our server</p>')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', Index),
     path('api/account/', include('account.urls')),
     path('api/courses/', include('course.urls'))
 ]
