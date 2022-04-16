60dce55bb5c2b78f45d8e718b45c6b72dc5795a1
jafarjtown <jafaridris82@gmail.com>
2022-04-16 15:05:08 +0100

    authentication++

--- platon_backend/urls.py
+++ platon_backend/urls.py
@@ -6,7 +6,7 @@ def Index(request):
     return HttpResponse('<h1>PlatON Server</h1> <br/> <p>welcome to our server</p>')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', Index),
+    path('', Index),
     path('api/account/', include('account.urls')),
     path('api/courses/', include('course.urls'))
 ]
